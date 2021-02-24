6f4983ee4008122be6411c123f829f76d46701a7
Yoke <tayok@synspective.com>
2021-02-24 18:28:00 +0900

    submit if_study

--- if_study/js/main.js
+++ if_study/js/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
 {
-    console.log('aa')
+    let i = 1;
+
+    while (i < 100) {
+        
+        if (i % 15 === 0) {
+            console.log(`FizzBuss ${i}`);
+        } else if (i % 3 === 0) {
+            console.log(`Fizz ${i}`);
+        } else if (i % 5 === 0) {
+            console.log(`Buzz ${i}`);
+        }
+
+        i += 1;
+    }
 }
