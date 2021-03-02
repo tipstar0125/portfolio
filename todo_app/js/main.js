d7f41b3fe03f8b917f2db22729cd3be1e1e0305b
Yoke <tayok@synspective.com>
2021-03-02 14:35:38 +0900

    modified todo_app to todo_object and displayTodos

--- todo_app/js/main.js
+++ todo_app/js/main.js
@@ -3,38 +3,68 @@
 {
     
     const todoNode = document.getElementById('todo');
-    const taskNodes = [];
+    const todos = [];
+    let taskID = 0;
+
+    function deleteView() {
+
+        const outputNodes = document.querySelectorAll('tbody>tr');
+        const tableBodyNode = document.getElementById('todo');
+
+        if (outputNodes) {
+            outputNodes.forEach(outputNode => {
+                tableBodyNode.removeChild(outputNode);
+            });
+        }
+    }
+
+    function displayTodos (todos) {
+
+        deleteView();
+
+        todos.forEach(todo => {
+
+            const taskNode = {
+                row: document.createElement('tr'),
+                idNode: document.createElement('td'),
+                commentNode: document.createElement('td'),
+                statusNode: document.createElement('td'),
+                deleteNode: document.createElement('td'),
+                statusButtonNode: document.createElement('button'),
+                deleteButtonNode: document.createElement('button'),
+            };
+
+            taskNode.idNode.textContent = todo.id;
+            taskNode.commentNode.textContent = todo.task;
+            task.value = '';
+            taskNode.statusButtonNode.textContent = todo.status;
+            taskNode.deleteButtonNode.textContent = '削除';
+    
+            taskNode.statusNode.appendChild(taskNode.statusButtonNode);
+            taskNode.deleteNode.appendChild(taskNode.deleteButtonNode);
+            taskNode.row.appendChild(taskNode.idNode);
+            taskNode.row.appendChild(taskNode.commentNode);
+            taskNode.row.appendChild(taskNode.statusNode);
+            taskNode.row.appendChild(taskNode.deleteNode);
+            todoNode.appendChild(taskNode.row);
+
+        });
+
+    }
 
     document.getElementById('add').addEventListener('click', () => {
     
         const task = document.getElementById('task');
-
-        const taskNode = {
-            row: document.createElement('tr'),
-            idNode: document.createElement('td'),
-            commentNode: document.createElement('td'),
-            statusNode: document.createElement('td'),
-            deleteNode: document.createElement('td'),
-            statusButtonNode: document.createElement('button'),
-            deleteButtonNode: document.createElement('button'),
+        const todo = {
+            id: taskID,
+            task: task.value,
+            status: '作業中'
         };
 
-        taskNodes.push(taskNode);
-
-        taskNode.idNode.textContent = taskNodes.length-1;
-        taskNode.commentNode.textContent = task.value;
-        task.value = '';
-        taskNode.statusButtonNode.textContent = '作業中';
-        taskNode.deleteButtonNode.textContent = '削除';
-
-        taskNode.statusNode.appendChild(taskNode.statusButtonNode);
-        taskNode.deleteNode.appendChild(taskNode.deleteButtonNode);
-        taskNode.row.appendChild(taskNode.idNode);
-        taskNode.row.appendChild(taskNode.commentNode);
-        taskNode.row.appendChild(taskNode.statusNode);
-        taskNode.row.appendChild(taskNode.deleteNode);
-        todoNode.appendChild(taskNode.row);
-        
+        taskID++;
+        todos.push(todo);
+        displayTodos(todos);
+      
     });
 
 }
