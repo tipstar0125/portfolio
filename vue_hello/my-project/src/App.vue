c877df86aa34be16efeb9c9d5ea3b0667dc76f90
Yoke <tayok@synspective.com>
2021-03-15 13:52:42 +0900

    hello world

--- vue_hello/my-project/src/App.vue
+++ vue_hello/my-project/src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>
+      {{ msg }}
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      msg: 'Hello World!!'
+    }
+  },
 }
 </script>
 
