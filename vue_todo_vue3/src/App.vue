2c5e35bf1bb69e6e11e128dea733fcdc3479a293
Yoke <tayok@synspective.com>
2021-04-07 16:08:38 +0900

    add vue_todo_vue3 to study composition api and typescript

---
+++ vue_todo_vue3/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TodoList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TodoList from './components/TodoList.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TodoList
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ vue_todo_vue3/src/components/TodoList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="todo">
+    <h2>ToDoリスト</h2>
+
+    <label><input type="radio" name="status" value="all" checked>すべて</label>
+    <label><input type="radio" name="status" value="working">作業中</label>
+    <label><input type="radio" name="status" value="completed">完了</label>
+
+    <table>
+      <thead>
+        <tr>
+            <th>ID</th>
+            <th>コメント</th>
+            <th>状態</th>
+            <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(todo, index) in todos" :key="'todo' + index">
+          <td>{{ index }}</td>
+          <td>{{ todo.task }}</td>
+          <td><button>{{ todo.isDone ? '完了' : '作業中' }}</button></td>
+          <td><button>削除</button></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h3>新規タスクの追加</h3>
+    <input type="text" v-model="newTask" style="width: 300px;">
+    <input type="button" value="追加" @click="addTask">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { Todo } from '@/types/todo.interface';
+
+export default defineComponent({
+  
+  setup() {
+    const newTask = ref<string>();
+    const todos = ref<Todo[]>([]);
+
+    const addTask = (): void => {
+      if (newTask.value) {
+        const todo: Todo = {
+          task: newTask.value,
+          isDone: false
+        }
+        todos.value.push(todo);
+        newTask.value = '';
+      }
+    } 
+
+    return {
+      newTask,
+      todos,
+      addTask,
+    }
+  }
+});
+</script>
+
+<style scoped>
+</style>
