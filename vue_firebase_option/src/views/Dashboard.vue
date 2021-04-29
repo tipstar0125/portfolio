6fa1f2b84217d226c4b5d19ec6d1246742f4d329
Yoke <tayok@synspective.com>
2021-04-29 15:48:43 +0900

    making dashboared

--- vue_firebase_option/src/views/Dashboard.vue
+++ vue_firebase_option/src/views/Dashboard.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="dashboard">
     <img class="mt-5" alt="Vue logo" src="../assets/logo.png">
-    <h1>ようこそ{{ user }}さん</h1>
+    <div class="container">
+      <div class="row">
+        <p class="col-sm-6 text-sm-left mb-3">ようこそ{{ user }}さん</p>
+        <p class="col-sm-6 text-sm-right mb-3">残高：{{ balance}}</p>
+      </div>
+      <button @click="getBalance" class="btn btn-primary">Get</button>
+      <button @click="addBalance" class="btn btn-primary">Add</button>
+      {{ fire_data}}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
@@ -14,19 +23,52 @@ export default {
   data() {
     return {
       user: '',
+      balance: 1000,
+      fire_data: {},
     }
   },
   created() {
     
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-          this.user = this.$store.getters['user/user']
-          console.log('sign in')
-          console.log(user.displayName)
+          const currentUser = this.$store.getters['user/user']
+          if (!currentUser) {
+            this.user = user.displayName
+          } else {
+            this.user = currentUser
+          }
       } else {
-        console.log('not sign in')
+        this.$router.push('/signin')
       }
     })
+  },
+  methods: {
+    getBalance() {
+      const url = 'https://vue-firebase-fcb7c-default-rtdb.firebaseio.com/uid.json'
+      axios.get(url).then((result) => {
+
+        if (result.data !== null) {
+          this.fire_data = result.data
+        }
+        
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    addBalance() {
+      const url = 'https://vue-firebase-fcb7c-default-rtdb.firebaseio.com/uid/ddd.json'
+      const item = {
+        'balance': 3000,
+      }
+      axios.put(url, item)
+    },
   }
 }
 </script>
+
+
+<style scoped>
+h1, p {
+  font-size: 24px;
+}
+</style>
