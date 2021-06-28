4c31079983b9f10f375c5dbc60a1a3997c067f98
Yoke <tayok@synspective.com>
2021-06-28 15:54:21 +0900

    modified html-css2 and add accordion

---
+++ accordion/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded",() => {
+  const title = document.querySelectorAll('.js-accordion-title');
+  for (let i = 0; i < title.length; i++){
+    let titleEach = title[i];
+    let content = titleEach.nextElementSibling;
+    titleEach.addEventListener('click', () => {
+      content.classList.remove('initial')
+      content.classList.toggle('is-open');
+      const isOpen = content.classList.contains('is-open');
+      if (!isOpen) {
+        content.classList.add('is-close');
+      } else {
+        content.classList.remove('is-close');
+      }
+    });
+  }
+});
